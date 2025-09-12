0b245e123367a4f3568f553ae79a69c31555daf1
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-09-12 12:19:39 +0000

    database setup

---
+++ backend/src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+import {ENV} from './env.js'
+
+const connectDB=async ()=>{
+    try {
+        const conn=await mongoose.connect(ENV.MONGODB_URI)
+        console.log(`MONGODB connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.log("MONGODB connection fail", error)
+        process.exit(1)
+    }
+}
+export default connectDB
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import express from "express"
 import {ENV} from "./config/env.js"
+import connectDB from "./config/db.js"
 const app=express()
+
+
 
 app.get("/",(req,res)=>{
     res.send("Hello")
@@ -8,4 +11,5 @@ app.get("/",(req,res)=>{
 
 app.listen(ENV.PORT,()=>{
     console.log(`Server is running on PORT : ${ENV.PORT}`)
+    connectDB()
 })
