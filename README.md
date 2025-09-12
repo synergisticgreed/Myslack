cfd7a1b9bf4f7ec3e4020b8301dc4410fdc7d8c9
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-09-12 13:06:41 +0000

    tools setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Myslack
+
+# backend installations
+- npm i express mongoose cors dotenv
+- @clerk/express@1.7.4
+
+# frontend installations
+-
--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,6 +5,13 @@ export const ENV={
    
         PORT : process.env.PORT || 5001,
         MONGODB_URI: process.env.MONGODB_URI,
-        NODE_ENV : process.env.NODE_ENV
+        NODE_ENV : process.env.NODE_ENV,
+        CLERK_PUBLISHABLE_KEY : process.env.CLERK_PUBLISHABLE_KEY,
+        CLERK_SECRET_KEY :  process.env.CLERK_SECRET_KEY,
+        STREAM_API_KEY : process.env.STREAM_API_KEY,
+        STREAM_API_SECRET : process.env.STREAM_API_SECRET,
+        SENTRY_DSN : process.env.SENTRY_DSN,
+        INGEST_EVENT_KEY : process.env.INGEST_EVENT_KEY,
+        INGEST_SIGNING_KEY : process.env.INGEST_SIGNING_KEY
   
 }
