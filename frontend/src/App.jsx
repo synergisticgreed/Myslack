0528bfc5b93d7eba715ac22d25a9f14fdf9148b6
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-09-13 10:19:25 +0000

    frontend authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+  return <header>
+      <SignedOut>
+        <SignInButton mode="modal"/>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
 }
-
+//we want to user created or deleted in sync with the database-->use inngest
+//when user.created => inngest will create the user in mongodb
+//when user.deleted => inngest will delete the user in mongodb
 export default App
