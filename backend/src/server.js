57508a1b4b44400d67b31557aa50cffc2fcd6346
Sameer Gupta <156401862+synergisticgreed@users.noreply.github.com>
2025-09-13 10:26:13 +0000

    clerk middleware added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from "express"
 import {ENV} from "./config/env.js"
 import connectDB from "./config/db.js"
+import {clerkMiddleware} from "@clerk/express"
+
+
 const app=express()
 
-
+app.use(clerkMiddleware())  //req.auth will be available to chack if user is authenticated or not
 
 app.get("/",(req,res)=>{
     res.send("Hello")
